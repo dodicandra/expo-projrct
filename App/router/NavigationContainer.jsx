a750fbd0f9b478273111ba264f7a1d8319903eb2
cekiddot <cekiddot@gmail.com>
2020-04-01 10:04:48 +0700

    rebase form HOTFIX

--- App/router/NavigationContainer.jsx
+++ App/router/NavigationContainer.jsx
@@ -2,7 +2,6 @@ import React, { useContext } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthStack from './authStack';
 import BottomNavigator from './BottomNavigator';
-<<<<<<< HEAD
 import { useInit, AuthContext } from '../context/hooks';
 
 export default function RouteContainer() {
@@ -12,17 +11,5 @@ export default function RouteContainer() {
     <NavigationContainer>
       {state.token !== null ? <BottomNavigator /> : <AuthStack />}
     </NavigationContainer>
-=======
-import HomeStack from './HomeStack';
-import AuthProvider, { AuthContext } from '../context/authContext';
-
-export default function RouteContainer() {
-  return (
-    <AuthProvider>
-      <NavigationContainer>
-        <AuthStack />
-      </NavigationContainer>
-    </AuthProvider>
->>>>>>> add login
   );
 }
