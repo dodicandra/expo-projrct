ec88ca5add350941a94fb2570e10bd5b9aa6f3f7
cekiddot <cekiddot@gmail.com>
2020-04-03 11:59:13 +0700

    solve bug loading

--- App/context/Provider/DataProvider.js
+++ App/context/Provider/DataProvider.js
@@ -1,15 +1,12 @@
 import React, { useReducer, useMemo } from 'react';
 import { dateRducer, dataInit, DataContext } from '../hooks';
-import * as api from '../../Api';
 
 const DataProvider = ({ children }) => {
   const [state, dispatch] = useReducer(dateRducer, dataInit);
 
   const setData = async (data) => {
     try {
-      setTimeout(() => {
-        dispatch({ type: 'SET_DATA', payload: data });
-      }, 2000);
+      dispatch({ type: 'SET_DATA', payload: data });
       dispatch({ type: 'STOP_LOADING' });
     } catch (error) {
       console.log('=>', error);
